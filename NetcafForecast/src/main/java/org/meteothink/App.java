f58e484d1c2be6897e37858090ab8446fa59b0d1
longyangzz <longyangzz@126.com>
2019-12-12 15:26:53 +0800

    获取经纬度成功

--- NetcafForecast/src/main/java/org/meteothink/App.java
+++ NetcafForecast/src/main/java/org/meteothink/App.java
@@ -1,5 +1,6 @@
 package org.meteothink;
 
+import org.meteoinfo.data.GridData;
 import org.meteoinfo.data.meteodata.MeteoDataInfo;
 
 /**
@@ -15,5 +16,8 @@ public class App
         String path = "D:\\2019\\Z_NWGD_C_BEHK_20180707060034_P_RFFC_SPCC-EDA10_201807070800_00101.GRB2";
         MeteoDataInfo aDataInfo = new MeteoDataInfo();
         aDataInfo.openNetCDFData(path);
+
+        String vName = "u-component_of_wind_height_above_ground";
+        GridData _gridData = aDataInfo.getGridData(vName);
     }
 }
