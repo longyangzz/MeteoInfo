750b3b62ac07c438aa5a39791b8e69be58beacf1
longyangzz <longyangzz@126.com>
2019-12-12 10:49:31 +0800

    Neccdf调用lib包解析grib测试成功

---
+++ NetcafForecast/src/main/java/org/meteothink/App.java
@@ -0,0 +1,19 @@
+package org.meteothink;
+
+import org.meteoinfo.data.meteodata.MeteoDataInfo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        String path = "D:\\2019\\Z_NWGD_C_BEHK_20180707060034_P_RFFC_SPCC-EDA10_201807070800_00101.GRB2";
+        MeteoDataInfo aDataInfo = new MeteoDataInfo();
+        aDataInfo.openNetCDFData(path);
+    }
+}
