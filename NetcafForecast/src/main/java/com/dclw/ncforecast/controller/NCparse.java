c1470ec4f1ea5c6413e5333384757d96b9722cca
longyangzz <longyangzz@126.com>
2019-12-12 18:50:44 +0800

    测试提价metroinfolib依赖成功

--- NetcafForecast/src/main/java/com/dclw/ncforecast/controller/NCparse.java
+++ NetcafForecast/src/main/java/com/dclw/ncforecast/controller/NCparse.java
@@ -1,6 +1,8 @@
 package com.dclw.ncforecast.controller;
 
 import com.dclw.ncforecast.biz.ReadGribToSqlBIZ;
+import org.meteoinfo.data.GridData;
+import org.meteoinfo.data.meteodata.MeteoDataInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,13 @@ public class NCparse {
 
     @RequestMapping("readGribToSql")
     public String readGribToSql(){
+        String path = "D:\\2019\\Z_NWGD_C_BEHK_20180707060034_P_RFFC_SPCC-EDA10_201807070800_00101.GRB2";
+        MeteoDataInfo aDataInfo = new MeteoDataInfo();
+        aDataInfo.openNetCDFData(path);
+
+        String vName = "u-component_of_wind_height_above_ground";
+        GridData _gridData = aDataInfo.getGridData(vName);
+
         Integer lt = readGribToSqlBIZ.inserGribDataToSql();
 
         return "hello";
