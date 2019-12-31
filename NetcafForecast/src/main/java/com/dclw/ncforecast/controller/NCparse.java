92d109a3f44d12f5d0cfaae935dbc6dee631c9a6
longyangzz <longyangzz@126.com>
2019-12-31 18:48:42 +0800

    指定 points坐标 为指定的坐标读取历史数据写出到数据库 测试成功

--- NetcafForecast/src/main/java/com/dclw/ncforecast/controller/NCparse.java
+++ NetcafForecast/src/main/java/com/dclw/ncforecast/controller/NCparse.java
@@ -37,9 +37,11 @@ public class NCparse {
 
     @Value("${custom.bound}")
     private String customBound;
-
-
     private List<Point2D> boundArr ;
+
+    @Value("${custom.points}")
+    private String customPoints;
+    private List<Point2D> customPointsArr ;
 
 
     @RequestMapping("readGribToSql")
@@ -80,8 +82,7 @@ public class NCparse {
             return log;
         }
 
-        //!! 针对目录
-        //! isDigui;
+        //!! 判断解析类型，是实时还是历史
         String isDigui = diguiTypeValue;
         int subfolderNum = 0;
         if(isDigui.equals("history"))
@@ -121,14 +122,35 @@ public class NCparse {
             boundArr.add(new Point2D(Double.parseDouble(curPos[0]), Double.parseDouble(curPos[1])) );
         }
 
+        //！ 解析获取points序列
+        int pointsLength = customPoints.split(";").length;
+        customPointsArr = new ArrayList<Point2D>();
+        for(int i = 0; i < pointsLength; ++i)
+        {
+            String [] curPos = customPoints.split(";")[i].split(",");
+            if(curPos.length != 2)
+            {
+                continue;
+            }
+            customPointsArr.add(new Point2D(Double.parseDouble(curPos[0]), Double.parseDouble(curPos[1])) );
+        }
+
         //! 遍历subPathList下的所有grib文件
+        //！ 按bound解析还是按points解析
         for ( String gribFolder : subPathList ) {
             File[] gribFiles = new File( gribFolder ).listFiles();
 
             for ( File gribFile : gribFiles ) {
                 if ( !gribFile.isDirectory() ) {
                     log += "解析文件开始" + gribFile.getPath()  + "\r\n";
-                    List<String> cunRecoords = OpenGribToSql(gribFile.getPath());
+                    List<String> cunRecoords;
+                    //! 指定点坐标为空，则按bound解析
+                    if(customPoints.isEmpty() ){
+                        cunRecoords = OpenGribToSqlByBound(gribFile.getPath());
+                    }else {
+                        cunRecoords = OpenGribToSqlByPoints(gribFile.getPath());
+                    }
+
 
                     if(cunRecoords.size() > 0){
                         log += "解析到有效数据在文件" + gribFile.getPath()  + "\r\n";
@@ -167,8 +189,8 @@ public class NCparse {
         return log;
     }
 
-    //根据指定的文件名，读取并判断类型，解析到数据库中
-    public List<String> OpenGribToSql(String fileName)
+    //根据指定的文件名，以及bound值读取并判断类型，解析到数据库中
+    public List<String> OpenGribToSqlByBound(String fileName)
     {
         List<String> srList = new ArrayList<String >();
         String stateValues = new String();
@@ -339,4 +361,166 @@ public class NCparse {
         }
 
     }
+
+    //根据指定的文件名，以及bound值读取并判断类型，解析到数据库中
+    public List<String> OpenGribToSqlByPoints(String fileName)
+    {
+        List<String> srList = new ArrayList<String >();
+        String stateValues = new String();
+
+
+        String baseName = new File(fileName).getName();
+        String [] nameList = baseName.split("_");
+        //！ 从文件名称解析生成三个时间
+        SimpleDateFormat dateInFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+        SimpleDateFormat dateInFormat1 = new SimpleDateFormat("yyyyMMddHHmm");
+        SimpleDateFormat dateOutFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+
+        /**
+         * 字符串转化为时间
+         */
+        Date date1=null;
+        try {
+            String genTime = nameList[4];
+            date1=dateInFormat.parse(genTime);
+            genTime = dateOutFormat.format(date1);
+
+            String startForecastTime = nameList[8];
+            date1=dateInFormat1.parse(startForecastTime);
+            startForecastTime = dateOutFormat.format(date1);
+
+            int addHour = Integer.parseInt(nameList[9].substring(0,3));
+            Calendar rightNow = Calendar.getInstance();
+            rightNow.setTime(date1);
+            rightNow.add(Calendar.HOUR,addHour);
+            date1 = rightNow.getTime();
+            String forecastTime = dateOutFormat.format(date1);
+
+            // ！ EDA10判断数据文件类型
+            if(fileName.endsWith("GRB2") && fileName.contains("EDA10") && dataFeatrue.contains("EDA10"))
+            {
+                //String path = forecastData + "\\20180707\\ShortTerm\\Z_NWGD_C_BEHK_20180707060054_P_RFFC_SPCC-EDA10_201807070800_00301.GRB2";
+                String path = fileName;
+                MeteoDataInfo aDataInfo = new MeteoDataInfo();
+                aDataInfo.openNetCDFData(path);
+
+                String uName = "u-component_of_wind_height_above_ground";
+                GridData gridDataU = aDataInfo.getGridData(uName);
+
+                String vName = "v-component_of_wind_height_above_ground";
+                GridData gridDataV = aDataInfo.getGridData(vName);
+
+                if(gridDataU.equals(null) || gridDataV.equals(null))
+                {
+                    aDataInfo.close();
+                    return  srList;
+                }
+
+                //! 最后一条记录和其它记录有末尾是否加逗号的区别
+                String fs = "";
+
+                int xNum = gridDataV.xArray.length;//gridDataV.xArray.length;
+                int yNUm = gridDataV.yArray.length;
+                int tNum = (xNum / (xyfblstep+1) + 1 ) * (yNUm / (xyfblstep+1) + 1);
+
+                int count = 0;
+                //！ 遍历点个数，根据点坐标，获取行列号，从行列号中取出相应数值写出，超过1000个则
+                //! 重新组装
+                int ptNums = customPointsArr.size();
+                for(int pi = 0; pi < ptNums; ++pi) {
+                    //! 经纬度索引号按行拍
+                    float lon = (float)customPointsArr.get(pi).getX();
+                    float lat = (float)customPointsArr.get(pi).getY();
+
+                    //! 获取行列号值
+                    int row = (int) ((lon - gridDataU.xArray[0]) / gridDataU.getXDelt());
+                    int col = (int) ((lat - gridDataU.yArray[0]) / gridDataU.getYDelt());
+
+                    int index = xNum * row + col;
+                    float uValue = (float)( gridDataU.toStation(lon, lat));
+                    float vValue = (float)( gridDataV.toStation(lon, lat));
+
+                    if (count == ptNums - 1 || ((count+1)%1000 == 0) ) {
+                        fs = String.format("('%s', '%s', '%s', %.2f, %.2f, %.2f, %.2f, %s)", genTime, startForecastTime, forecastTime, uValue, vValue, lon, lat, Integer.toString(index));
+                        stateValues += fs;
+                        srList.add(stateValues);
+
+                        //! 清空
+                        stateValues = "";
+                    } else {
+                        fs = String.format("('%s', '%s', '%s', %.2f, %.2f, %.2f, %.2f, %s),", genTime, startForecastTime, forecastTime, uValue, vValue, lon, lat, Integer.toString(index));
+                        stateValues += fs;
+                    }
+
+                    count++;
+                }
+                aDataInfo.close();
+            }
+
+            if(fileName.endsWith("GRB2") && fileName.contains("WWD") && dataFeatrue.contains("WWD"))
+            {
+                //String path = forecastData + "\\20180707\\ShortTerm\\Z_NWGD_C_BEHK_20180707060054_P_RFFC_SPCC-EDA10_201807070800_00301.GRB2";
+                String path = fileName;
+                MeteoDataInfo aDataInfo = new MeteoDataInfo();
+                aDataInfo.openNetCDFData(path);
+
+                String uName = "u-component_of_wind_altitude_above_msl";
+                GridData gridDataU = aDataInfo.getGridData(uName);
+
+                if(gridDataU.equals(null))
+                {
+                    aDataInfo.close();
+                    return  srList;
+                }
+
+                //! 最后一条记录和其它记录有末尾是否加逗号的区别
+                String fs = "";
+
+                int xNum = gridDataU.xArray.length;//gridDataV.xArray.length;
+                int yNUm = gridDataU.yArray.length;
+                int tNum = (xNum / (xyfblstep+1) + 1 ) * (yNUm / (xyfblstep+1) + 1);
+
+                // 遍历维度层
+                int count = 0;
+                //！ 遍历点个数，根据点坐标，获取行列号，从行列号中取出相应数值写出，超过1000个则
+                //! 重新组装
+                int ptNums = customPointsArr.size();
+                for(int pi = 0; pi < ptNums; ++pi) {
+                    //! 经纬度索引号按行拍
+                    float lon = (float)customPointsArr.get(pi).getX();
+                    float lat = (float)customPointsArr.get(pi).getY();
+
+                    //! 获取行列号值
+                    int row = (int) ((lon - gridDataU.xArray[0]) / gridDataU.getXDelt());
+                    int col = (int) ((lat - gridDataU.yArray[0]) / gridDataU.getYDelt());
+
+                    int index = xNum * row + col;
+                    float uValue = (float)( gridDataU.toStation(lon, lat));
+
+                    if (count == ptNums - 1 || ((count+1)%1000 == 0) ) {
+                        fs = String.format("('%s', '%s', '%s', %.2f, %.2f, %.2f, %.2f, %s)", genTime, startForecastTime, forecastTime, uValue, 0.0, lon, lat, Integer.toString(index));
+                        stateValues += fs;
+                        srList.add(stateValues);
+
+                        //! 清空
+                        stateValues = "";
+                    } else {
+                        fs = String.format("('%s', '%s', '%s', %.2f, %.2f, %.2f, %.2f, %s),", genTime, startForecastTime, forecastTime, uValue, 0.0, lon, lat, Integer.toString(index));
+                        stateValues += fs;
+                    }
+
+                    count++;
+                }
+                aDataInfo.close();
+            }
+
+
+            return  srList;
+
+        } catch (ParseException e) {
+            return  srList;
+        }
+
+    }
+
 }
